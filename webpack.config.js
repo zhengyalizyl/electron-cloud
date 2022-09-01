a2cb155525f4ba27d0c2d142658d5f23a1aa30a8
lizheng <v-lizheng@expediagroup.com>
2022-09-01 20:36:24 +0800

    add update

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,12 @@ const path = require("path");
 
 module.exports = {
     target: "electron-main",
-    entry: "./main.js"
+    entry: "./main.js",
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'main.js'
+    },
+    node: {
+        __dirname: false
+    }
 }
